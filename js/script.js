dbff4c6d6c7dbb36dd4cac5668c900db739869f1
Nii Derry <derricktagoe@gmail.com>
2023-10-20 11:57:37 +0000

    added a smooth scroll to the sections

--- js/script.js
+++ js/script.js
@@ -15,6 +15,35 @@ btnNavEl.addEventListener("click", function () {
   headerEL.classList.toggle("nav-open");
 });
 
+// /////////////////////////////////////////////////
+//  smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // scrool back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // scroll to other sections
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // close mobile nav
+    if (link.classList.contains("main-nav-link"))
+      headerEL.classList.toggle("nav-open");
+  });
+});
+
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
